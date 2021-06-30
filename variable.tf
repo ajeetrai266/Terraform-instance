ab7320c5307c824767e84057ff6951a8b22a4d1d
Ajeet Rai <65476458+ajeetrai266@users.noreply.github.com>
2021-06-30 13:34:57 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,12 @@
+variable "n" {
+    type = number
+    default = 2
+    description = "No. of Worker Node"
+}
+
 variable "sg" {
     type = list
     default = ["launch-wizard-15"]
+    description = "AWS Security Group list"
 }
+
