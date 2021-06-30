b62c5ed7b530c47b44e244d87ff474c4287b56d0
Ajeet Rai <65476458+ajeetrai266@users.noreply.github.com>
2021-06-30 17:33:59 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -66,8 +66,8 @@ resource "azurerm_linux_virtual_machine" "os1" {
   source_image_reference {
      publisher = "RedHat"
        offer     = "RHEL"
-       sku       = "7.6"
-       version   = "7.6.2019052206"
+       sku       = "8.2"
+       version   = "latest"
   }
 
   admin_ssh_key {
